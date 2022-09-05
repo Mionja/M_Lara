2137c80b9dfab29b7ae1ef76bb2ae805a042ff88
RANAIVOARISON Mionja <mionjaran2005@gmail.com>
2022-09-04 18:50:56 -0600

    start project

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>M_lara</title>
+
+    <link href="{{ asset('css/all.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/css/ionicons.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bracket.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  </head>
+
+  <body>
+
+@include('flash-message')
+@yield('section')
+    
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/datepicker.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('js/moment.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.peity.min.js') }}"></script>
+
+    <script src="{{ asset('js/bracket.js') }}"></script>
+  </body>
+
+</html>
